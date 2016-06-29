884b5d36ec33b8e77981449de7690a9a237b0734
Kate Nelson <mkatenelson@gmail.com>
2016-06-28 19:54:38 -0700

    messaging structure

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628023656) do
+ActiveRecord::Schema.define(version: 20160629020932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20160628023656) do
     t.boolean  "writing"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "foods", force: :cascade do |t|
@@ -50,6 +57,18 @@ ActiveRecord::Schema.define(version: 20160628023656) do
     t.integer  "blocker_id"
     t.integer  "status"
   end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.boolean  "read",            default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "musics", force: :cascade do |t|
     t.boolean  "pop"
