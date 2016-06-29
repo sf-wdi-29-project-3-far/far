c0d48d337a1a69f7276f89942daa806711c710fb
kabah <1kabah@gmail.com>
2016-06-28 21:03:58 -0700

    debugs messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def authenticate_user
+    if !current_user
+      redirect_to sign_in_path, notice: "You must be signed in to do that!"
+    end
+  end
+  
 end
