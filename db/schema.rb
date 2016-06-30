9bd1a43442731bf390a0f8fd8e3fdb48dc9b2cad
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-06-29 18:11:48 -0700

    fixes migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160629231807) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,13 +25,6 @@ ActiveRecord::Schema.define(version: 20160629231807) do
     t.boolean  "writing"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "conversations", force: :cascade do |t|
-    t.integer  "sender_id"
-    t.integer  "recipient_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "foods", force: :cascade do |t|
@@ -60,20 +51,6 @@ ActiveRecord::Schema.define(version: 20160629231807) do
     t.integer  "status"
   end
 
-<<<<<<< HEAD
-  create_table "messages", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "conversation_id"
-    t.integer  "user_id"
-    t.boolean  "read",            default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
-  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
-
-=======
   create_table "matches", force: :cascade do |t|
     t.string   "origin_country"
     t.string   "current_country"
@@ -87,7 +64,18 @@ ActiveRecord::Schema.define(version: 20160629231807) do
     t.text     "interests",       default: [],              array: true
   end
 
->>>>>>> search
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.boolean  "read",            default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
+
   create_table "musics", force: :cascade do |t|
     t.boolean  "pop"
     t.boolean  "rock"
@@ -127,8 +115,8 @@ ActiveRecord::Schema.define(version: 20160629231807) do
     t.string   "current_city"
     t.string   "origin_city"
     t.integer  "age"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.integer  "art_id"
     t.integer  "music_id"
     t.integer  "food_id"
@@ -145,13 +133,9 @@ ActiveRecord::Schema.define(version: 20160629231807) do
     t.boolean  "male"
     t.boolean  "female"
     t.boolean  "other_gender"
-<<<<<<< HEAD
-=======
-    t.text     "languages",       default: [],              array: true
     t.text     "interests",       default: [],              array: true
     t.integer  "count",           default: 0
     t.string   "current_country"
->>>>>>> staging
   end
 
   add_index "users", ["art_id"], name: "index_users_on_art_id", using: :btree
